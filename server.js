c7d1a110d40c0d75de7089f057490ceb932543e6
y.zhai@partner.samsung.com <y.zhai@partner.samsung.com>
2017-09-21 11:46:25 -0400

    test2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+var port = process.env.PORT ||3000;
 
 const fs = require('fs');
 
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up on port 3000');
 });
